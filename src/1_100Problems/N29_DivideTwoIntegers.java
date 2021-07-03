9100708e893c83419d693bff26bb24757188569c
Leo <icpc0928@yahoo.com.tww>
2021-07-03 16:35:43 +0800

    20210703-5

--- src/1_100Problems/N29_DivideTwoIntegers.java
+++ src/1_100Problems/N29_DivideTwoIntegers.java
@@ -1,5 +1,6 @@
 public class N29_DivideTwoIntegers {
 
+    //不熟,只知道要用<<
 
     public static void main(String[] args){
         Solution solution = new Solution();
@@ -12,48 +13,37 @@ public class N29_DivideTwoIntegers {
 
     static class Solution {
         public int divide(int dividend, int divisor) {
+            //結果為正 or 負
             boolean positive = ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0));
-
-            if(dividend == Integer.MIN_VALUE && divisor == -1){
-                return Integer.MAX_VALUE;
-            } else if(divisor == Integer.MIN_VALUE && dividend != Integer.MIN_VALUE){
-                return 0;
-            }
-
-            //轉負數
-            if(dividend > 0){
-                dividend = ~dividend + 1;
-            }
-            if(divisor > 0){
-                divisor = ~divisor + 1;
-            }
-
-            int quotient = 0;
-
-            //做除法取商
-            while(dividend <= divisor){
-                int q = -1;
-                int r = divisor;
-                while(r > dividend){
-                    r = r << 1;     //除以2的1次方
-                    q = q << 1;
+            //將除數與被除數轉long
+            long dividendL = Math.abs((long)dividend);
+            long divisorL = Math.abs((long)divisor);
+            //
+            long res = 0;   //結果
+            //當被除數大於等於除數時
+            while(dividendL >= divisorL){
+                // 除數
+                long d = divisorL;
+                // 被除數>除數 至少有1個
+                long count=1;
+                // 當被除數 >= 兩倍的除數
+                while(dividendL >= (d << 1)){
+                    // 除數*2
+                    d = (d<<1);
+                    // 被除數包含除數的個數*2
+                    count = (count << 1);
                 }
-                if(r < dividend){
-                    r = r >> 1;     //乘以2的1次方
-                    q = q >> 1;
-                }
-                dividend -= r;
-                quotient += q;
+                // 個數累加
+                res += count;
+                // 被除數減去除數
+                dividendL -= d;
             }
-
-            //取正確的商
-            if(positive){
-                if(quotient == Integer.MIN_VALUE){
-                    quotient++;
-                }
-                quotient = ~quotient + 1;
+            // 如果返回结果大于int最大值
+            if(res > Integer.MAX_VALUE){
+                // 如果结果是正数，返回int最大值，反之返回int最小值
+                return positive ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            return quotient;
+            return positive ? (int)res : (int)-res;
 
         }
     }
