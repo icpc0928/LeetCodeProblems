9ea5c6906324238b76ebba93f478c960295a1822
Leo <icpc0928@yahoo.com.tww>
2021-08-29 19:04:01 +0800

    20210829

--- src/1_100Problems/N01_TwoSum.java
+++ src/1_100Problems/N01_TwoSum.java
@@ -16,25 +16,56 @@ public class N01_TwoSum {
             int[] solution = new int[2];
             HashMap<Integer, Integer> hashMap = new HashMap<>();
 
+/**
+ * containsKey 一層迴圈,原本怕key會重複 其實不會
+ */
             for(int i = 0; i < nums.length; i++){
-                hashMap.put(i, nums[i]);
-            }
-            for(int i = 0; i < nums.length; i++){
-                solution = new int[2];
-
-                if(hashMap.containsValue(target - hashMap.get(i))){
-                    solution[0] = i;
-                    System.out.println("HI");
-                    for(Integer key : hashMap.keySet()){
-                        if(hashMap.get(key) == (target - hashMap.get(i)) && key != i){
-                            solution[1] = key;
-                            return solution;
-                        }
-                    }
+                if(hashMap.containsKey(target - nums[i])){
+                    solution[0] = hashMap.get(target - nums[i]);
+                    solution[1] = i;
+                    break;
                 }
+                hashMap.put(nums[i], i);
             }
-
             return solution;
+
+
+            /**
+             * containsValue: 一樣兩層迴圈,要繞出key
+             */
+//            for(int i = 0; i < nums.length; i++){
+//                if(hashMap.containsValue(target - nums[i])){
+//                    for(Integer key : hashMap.keySet()){
+//                        if(hashMap.get(key) == (target - nums[i])){
+//                            solution[0] = key;
+//                            solution[1] = i;
+//                            break;
+//
+//                        }
+//                    }
+//                }
+//                hashMap.put(i, nums[i]);
+//            }
+            /**
+             * containsValue: 兩層迴圈分開做速度慢
+             */
+//            for(int i = 0; i < nums.length; i++){
+//                hashMap.put(i, nums[i]);
+//            }
+//            for(int i = 0; i < nums.length; i++){
+//                solution = new int[2];
+//
+//                if(hashMap.containsValue(target - hashMap.get(i))){
+//                    solution[0] = i;
+//                    System.out.println("HI");
+//                    for(Integer key : hashMap.keySet()){
+//                        if(hashMap.get(key) == (target - hashMap.get(i)) && key != i){
+//                            solution[1] = key;
+//                            return solution;
+//                        }
+//                    }
+//                }
+//            }
         }
     }
 }
